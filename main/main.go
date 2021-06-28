ce5ab1f3fe756d22e0cb286f1796962fe4ef4ecd
songhwee1 <songhwee1@naver.com>
2021-06-28 23:50:55 +0900

    add PoW

--- main/main.go
+++ main/main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/tensor-programming/golang-blockchain/blockchain"
 )
 
 func main() {
 	chain := blockchain.InitBlockChain()
+
 	chain.AddBlock("First Block after Genesis")
 	chain.AddBlock("Second Block aftrer Genesis")
 	chain.AddBlock("Third Block after Genesis")
 
 	for _, block := range chain.Blocks {
-		fmt.Println()
 		if len(block.PrevHash) < 1 {
 			fmt.Printf("Previous Hash: None\n")
 		} else {
@@ -22,5 +23,8 @@ func main() {
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
 
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
