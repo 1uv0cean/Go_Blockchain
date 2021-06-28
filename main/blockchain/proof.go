890793dcc32d089f2fe8e889a4b9bb49d23d6117
songhwee1 <songhwee1@naver.com>
2021-06-28 23:07:39 +0900

    add validate function

--- main/blockchain/proof.go
+++ main/blockchain/proof.go
@@ -61,7 +61,17 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 	fmt.Println()
 
 	return nonce, hash[:]
+}
 
+func (pow *ProofOfWork) Validate() bool {
+	var intHash big.Int
+
+	data := pow.InitData(pow.Block.Nonce)
+
+	hash := sha256.Sum256(data)
+	intHash.SetBytes(hash[:])
+
+	return intHash.Cmp(pow.Target) == -1
 }
 
 func ToHex(num int64) []byte {
