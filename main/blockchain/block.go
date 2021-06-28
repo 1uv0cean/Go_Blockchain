12adc35cd9c418fd0a7b7e78fae530b314a6cb20
songhwee1 <songhwee1@naver.com>
2021-06-28 22:53:19 +0900

    add nonce

--- main/blockchain/block.go
+++ main/blockchain/block.go
@@ -13,6 +13,7 @@ type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
+	Nonce    int
 }
 
 func (b *Block) DeriveHash() {
@@ -22,8 +23,13 @@ func (b *Block) DeriveHash() {
 }
 
 func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
+	block := &Block{[]byte{}, []byte(data), prevHash, 0}
+	pow := NewProof(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
 
